886942776e1077bab22f21c8f2a193ebb8fce2d3
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-04-02 09:31:18 +0000

    Moved sorrento-2007-sprint branch into obsolete/

---
+++ sorrento-2007-sprint/p4a/ploneaudio/__init__.py
@@ -0,0 +1,22 @@
+import p4a.ploneaudio.sitesetup
+
+def has_fatsyndication_support():
+    try:
+        import Products.fatsyndication
+    except ImportError, e:
+        return False
+    return True
+
+def has_ataudio_support():
+    try:
+        import Products.ATAudio
+        return True
+    except:
+        return False
+
+def has_blobfile_support():
+    try:
+        import Products.BlobFile
+    except ImportError, e:
+        return False
+    return True
