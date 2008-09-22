74e0d403491febb571754850d27ab3469a483ccc
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-09-22 14:19:08 +0000

    Fixed up version info and added GS profile for better QI naming.

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
-import sys, os
+import os
 
-version = '1.0'
+versionfile = open(os.path.join('p4a', 'ploneaudio', 'VERSION.txt'))
+version = versionfile.read().strip()
+versionfile.close()
+
 readme = open('README.txt')
 long_description = readme.read()
 readme.close()
@@ -17,7 +20,7 @@ setup(name='p4a.ploneaudio',
           'Programming Language :: Python',
           'License :: OSI Approved :: GNU General Public License (GPL)',
           'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: Multimedia :: Sound/Audio'
+          'Topic :: Multimedia :: Sound/Audio',
           ],
       keywords='Plone4Artists audio mp3 ogg podcasting podcast id3 music',
       author='Rocky Burt',
@@ -30,9 +33,6 @@ setup(name='p4a.ploneaudio',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          'p4a.audio>=1.0',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+          'p4a.audio>=1.1dev',
+          ],
       )
