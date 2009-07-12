1451bfda24ca1e78b5713c24e5aad01b1123dc4b
rosspatterson <rosspatterson@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-07-12 19:08:47 +0000

    Move catalog index and metadata setup into the GS profile.  Resolves an issue with refreshCatalog being called for very large sites.

--- p4a/ploneaudio/sitesetup.py
+++ p4a/ploneaudio/sitesetup.py
@@ -4,17 +4,14 @@ from p4a.common import site
 from p4a.z2utils import indexing
 from p4a.z2utils import utils
 
-from StringIO import StringIO
-
-from Products.CMFCore.utils import getToolByName, SimpleRecord 
-
-from p4a.audio.interfaces import IAudio
+from Products.CMFCore.utils import getToolByName
 
 import logging
 logger = logging.getLogger('p4a.ploneaudio.sitesetup')
 
 try:
     import five.localsitemanager
+    five.localsitemanager # pyflakes
     HAS_FLSM = True
     logger.info('Using five.localsitemanager')
 except ImportError, err:
@@ -23,9 +20,7 @@ except ImportError, err:
 def setup_portal(portal):
     site.ensure_site(portal)
     setup_site(portal)
-    setup_indexes(portal)
-    setup_metadata(portal)
-    setup_smart_folder_indexes(portal)
+    indexing.ensure_object_provides(portal)
 
     qi = getToolByName(portal, 'portal_quickinstaller')
     qi.installProducts(['CMFonFive'])
@@ -45,97 +40,6 @@ def setup_site(site):
         else:
             sm.registerUtility(interfaces.IAudioSupport,
                                content.AudioSupport('audio_support'))
-
-def setup_indexes(portal):
-    """Install specific indexes for the audio metadata fields
-    so they are searchable.
-
-    """
-
-    out = StringIO()
-    pc = getToolByName(portal, 'portal_catalog')
-
-    indexing.ensure_object_provides(portal)
-
-    if not 'audio_genre_id' in pc.indexes():
-        pc.addIndex('audio_genre_id', 'FieldIndex')
-        pc.manage_reindexIndex('audio_genre_id')
-        print >>out, 'The FieldIndex "audio_genre_id" was successfully created'
-
-    if not 'audio_artist' in pc.indexes():
-        extra = SimpleRecord(lexicon_id='plaintext_lexicon',
-                             index_type='Okapi BM25 Rank')
-
-        pc.addIndex('audio_artist', 'ZCTextIndex', extra)
-        pc.manage_reindexIndex('audio_artist')
-        print >>out, 'The ZCTextIndex "audio_artist" was successfully created'
-
-    if not 'audio_track' in pc.indexes():
-        pc.addIndex('audio_track', 'FieldIndex')
-        pc.manage_reindexIndex('audio_track')
-        print >>out, 'The FieldIndex "audio_track" was successfully created'
-
-    if not 'Format' in pc.indexes():
-        pc.addIndex('Format', 'FieldIndex')
-        pc.manage_reindexIndex('Format')
-        print >>out, 'The FieldIndex "Format" was successfully created'
-
-def setup_metadata(portal):
-    """Adds the specified columns to the catalog specified,
-       which must inherit from CMFPlone.CatalogTool.CatalogTool, or otherwise
-       use the Plone ExtensibleIndexableObjectWrapper."""
-
-    out = StringIO()
-    pc = getToolByName(portal, 'portal_catalog', None)
-
-    try:
-        pc.delColumn('audio_artist')
-    except:
-        pass
-
-    reindex = []
-
-    pc.manage_addColumn('audio_artist')
-    reindex.append('audio_artist')
-
-    if reindex:
-        pc.refreshCatalog()
-
-    print >>out, 'The metadata "audio_artist" was successfully added.'
-
-
-INDEX_MAPPING = {'audio_artist':
-                    {'name': 'Artist name',
-                     'description': 'The name of the artist.',
-                     'enabled': True,
-                     'criteria': ('ATSimpleStringCriterion',)},
-                 'audio_genre_id':
-                    {'name': 'Genre',
-                     'description': 'The genre id of the song.'
-                                    'this is a number 0-147. '
-                                    'See genre.py for the genre names.',
-                     'enabled': True,
-                     'criteria': ('ATSimpleIntCriterion',)},
-                 'Format':
-                    {'name': 'MIME Types',
-                     'description': 'The MIME type of the file. '
-                                 'For an MP3 file, this is audio/mpeg.',
-                     'enabled': True,
-                     'criteria': ('ATSimpleStringCriterion',)},
-                 }
-
-def setup_smart_folder_indexes(portal):
-    """Adds the default indexes to be available from smartfolders"""
-    atct_config = getToolByName(portal, 'portal_atct')
-
-    for index, index_info in INDEX_MAPPING.items():
-        atct_config.updateIndex(index, friendlyName=index_info['name'],
-                                description=index_info['description'],
-                                enabled=index_info['enabled'],
-                                criteria=index_info['criteria'])
-        atct_config.updateMetadata(index, friendlyName=index_info['name'],
-                                   description=index_info['description'],
-                                   enabled=True)
 
 def _cleanup_utilities(site):
     raise NotImplementedError('Current ISiteManager support does not '
